b4ff717fbd5cf24b861bc67e066de2feb08b27c2
1dg <ricky2425@naver.com>
2025-04-12 19:24:22 +0900

    feat: BoardList 생성하기

--- react-task-app/src/App.tsx
+++ react-task-app/src/App.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
 import { appContainer, board, buttons } from "./App.css"
+import BoardList from "./components/BoardList/BoardList"
 
 
 function App() {
+  const[ activeBoardId, setActiveBoardId ] = useState('board-0');
 
   return (
     <div className={appContainer}>
       <div className={board}>
-
+        <BoardList 
+          activeBoardId={activeBoardId}
+          setActiveBoardId={setActiveBoardId} />
       </div>
       <div className={buttons}>
         <button>
--- react-task-app/src/components/BoardList/BoardList.tsx
+++ react-task-app/src/components/BoardList/BoardList.tsx
@@ -1,8 +1,44 @@
 // import React from 'react'
 
-const BoardList = () => {
+import React, { FC, useState } from "react";
+import { useTypedSelector } from "../../hooks/redux";
+import SideForm from "./SideForm/SideForm";
+import { FiPlusCircle } from "react-icons/fi";
+
+type TBoardListProps = {
+    activeBoardId : string;
+    setActiveBoardId : React.Dispatch<React.SetStateAction<string>>;
+}
+
+const BoardList:FC<TBoardListProps> = ({
+    activeBoardId,
+    setActiveBoardId
+}) => {
+
+    const { boardArray } = useTypedSelector(state => state.boards);
+    const [isFormOpen, setIsFormOpen] =useState(false);
+
     return (
-        <div>BoardList</div>
+        <div>
+            <div>
+                게시판 : 
+            </div>
+            {boardArray.map((board,index) => (
+                <div key={board.boardId}>
+                    <div>
+                        {board.boardName}
+                    </div>
+                </div>
+            ))}
+            <div>
+                {
+                    isFormOpen ?
+                        <SideForm setIsFormOpen={setIsFormOpen} />
+                    :
+                        <FiPlusCircle onClick={() => setIsFormOpen(!isFormOpen)}/>
+                }
+            </div>
+        </div>
     )
 }
 
