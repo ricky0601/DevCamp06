af8a7ab9da7cba3832e6be0c4019fb2ca9a68f21
1dg <ricky2425@naver.com>
2025-04-16 21:02:35 +0900

    feat: LogItem 컴포넌트 생성하기

--- react-task-app/src/components/LoggerModal/LogItem/LogItem.tsx
+++ react-task-app/src/components/LoggerModal/LogItem/LogItem.tsx
@@ -1,8 +1,33 @@
 // import React from 'react'
 
-const LogItem = () => {
+import { FC } from "react";
+import { ILogItem } from "../../../types"
+import { BsFillPersonFill } from "react-icons/bs";
+
+type TLogItemProps = {
+    logItem: ILogItem;
+}
+
+const LogItem: FC<TLogItemProps> = ({
+    logItem
+}) => {
+
+    let timeOffset = new Date(Date.now() - Number(logItem.logTimestamp));
+
+    const showOffsetTiem = `${timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}m` : ""}
+    ${timeOffset.getSeconds() > 0 ? `${timeOffset.getSeconds()}s ago` : ""}
+    ${timeOffset.getSeconds() === 0 ? `just now` : ""}
+    `
+
     return (
-        <div>LogItem</div>
+        <div>
+            <div>
+                <BsFillPersonFill />
+                {logItem.logAuthor}
+            </div>
+            <div>{logItem.logMessage}</div>
+            <div>{showOffsetTiem}</div>
+        </div>
     )
 }
 
