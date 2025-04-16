0108fc94778bd81ee1a59d246cae68e402154980
1dg <ricky2425@naver.com>
2025-04-16 20:52:10 +0900

    feat: LoggerModal 컴포넌트 생성하기

--- react-task-app/src/App.tsx
+++ react-task-app/src/App.tsx
@@ -1,23 +1,26 @@
 import { useState } from "react"
-import { appContainer, board, buttons } from "./App.css"
+import { appContainer, board, buttons, deleteBoardButton, loggerButton } from "./App.css"
 import BoardList from "./components/BoardList/BoardList"
 import ListsContainer from "./components/ListsContainer/ListsContainer";
 import { useTypedSelector } from "./hooks/redux";
 import { IBoard } from "./types";
 import EditModal from "./components/EditModal/EditModal";
+import LoggerModal from "./components/LoggerModal/LoggerModal";
 
 
 function App() {
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
   const[ activeBoardId, setActiveBoardId ] = useState('board-0');
   const modalActive = useTypedSelector( state => state.boards.modalActive);
 
   const boards = useTypedSelector(state => state.boards.boardArray);
   const getActiveBoard = boards.filter((board : IBoard) => board.boardId === activeBoardId)[0];
+
   const lists = getActiveBoard.lists;
 
   return (
     <div className={appContainer}>
-      
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
       {modalActive ? <EditModal /> : null}
 
       <BoardList 
@@ -29,11 +32,11 @@ function App() {
       </div>
 
       <div className={buttons}>
-        <button>
+        <button className={deleteBoardButton}>
           이 게시판 삭제하기
         </button>
-        <button>
-          활동 목록 보이기
+        <button className={loggerButton} onClick={() => setIsLoggerOpen(!isLoggerOpen)}>
+          {isLoggerOpen ? "활동 목록 숨기기" : "활동 목록 보이기"}
         </button>
       </div>
     </div>
