1a4799c7572471c757c526e1ee1c45a55e55a09e
1dg <ricky2425@naver.com>
2025-04-21 21:31:23 +0900

    API 통신과 데이터 레이어

--- book-store/src/components/common/Header.tsx
+++ book-store/src/components/common/Header.tsx
@@ -1,28 +1,14 @@
 import { styled } from "styled-components";
 import logo from '../../assets/images/logo.png';
 import { Link } from "react-router-dom";
-
-const CATEGORY =[
-    {
-        id : null,
-        name: "전체",
-    },
-    {
-        id : 0,
-        name : "동화",
-    },
-    {
-        id : 1,
-        name : "소설",
-    },
-    {
-        id : 2,
-        name : "사회",
-    }
-];
-
+import { useEffect, useState } from "react";
+import { Category } from "../../models/category.model";
+import { fetchCategory } from "../../api/category.api";
+import { useCategory } from "../../hooks/useCategory";
 
 function Header() {
+
+    const {category} = useCategory();
     
     return(
         <HeaderStyle>
@@ -34,7 +20,7 @@ function Header() {
             <nav className="category">
                 <ul>
                     {
-                        CATEGORY.map((item) => (
+                        category.map((item) => (
                             <li key={item.id}>
                                 <Link to={item.id === null ? "/books" : `/books?category_id=${item.id}`}>
                                     {item.name}
