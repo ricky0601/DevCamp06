1c587937d2c33e2b9a1f2fb709f26fd406c0e93f
1dg <ricky2425@naver.com>
2025-04-22 21:53:59 +0900

    feat: 도서 목록 페이지 (2)

--- book-store/src/components/books/BookItem.tsx
+++ book-store/src/components/books/BookItem.tsx
@@ -37,6 +37,63 @@ function BookItem({book} : BookItemProps) {
     );
 }
 
-const BookItemStyle = styled.div``;
+const BookItemStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
+
+    .img{
+        border-radius: ${({theme}) => theme.borderRadius.default};
+        overflow: hidden;
+        img{
+            max-width: 100%;
+        }
+    }
+
+    .content{
+        padding : 16px;
+        position: relative;
+
+        .title{
+            font-size: 1.25rem;
+            font-weight: 700;
+            margin: 0 0 12px 0;
+        }
+        .summary{
+            font-size: 0.875rem;
+            color: ${({theme}) => theme.color.secondory};
+            margin: 0 0 4px 0;
+        }
+        .author{
+            font-size: 0.875rem;
+            color: ${({theme}) => theme.color.secondory};
+            margin: 0 0 4px 0;
+        }
+        .price{
+            font-size: 1rem;
+            color: ${({theme}) => theme.color.secondory};
+            margin: 0 0 4px 0;
+            font-weight: 700;
+        }
+        .likes{
+            display: inline-flex;
+            align-items: center;
+            gap: 4px;
+            font-size: 0.875rem;
+            color: ${({theme}) => theme.color.primary};
+            margin: 0 0 4px 0;
+            font-weight: 700;
+            border: 1px solid ${({theme}) => theme.color.border};
+            border-radius:  ${({theme}) => theme.borderRadius.default};
+            padding: 4px 12px;
+            position: absolute;
+            bottom: 16px;
+            right: 16px;
+            svg{
+                color: ${({theme}) => theme.color.primary};
+            }
+        }
+    }
+`;
 
 export default BookItem;
