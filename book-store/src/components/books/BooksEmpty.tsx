53d761e8f5a181d93b4d74edeaae710c16ca965d
1dg <ricky2425@naver.com>
2025-04-22 22:05:55 +0900

    feat: BooksEmpty, BooksFilter UI 구현

--- book-store/src/components/books/BooksEmpty.tsx
+++ book-store/src/components/books/BooksEmpty.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import styled from 'styled-components';
+import { FaFaceSmileWink } from 'react-icons/fa6';
+import Title from '../common/Title';
+import { Link } from 'react-router-dom';
 
 function BooksEmpty() {
     return (
-        <div>
-            
-        </div>
+        <BooksEmptyStyle>
+            <div className="icon">
+                <FaFaceSmileWink />
+            </div>
+            <Title size='large' color='secondory'>검색 결과가 없습니다.</Title>
+            <p>
+                <Link to="/books">
+                    전체 검색 결과로 이동
+                </Link>
+            </p>
+        </BooksEmptyStyle>
     );
 }
 
-const BooksEmptyStyle = styled.div``;
+const BooksEmptyStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 12px;
+    padding: 120px 0;
+
+    .icon{
+        svg{
+            font-size: 4rem;
+            fill: #ccc;
+        }
+    }
+`;
 
 export default BooksEmpty;
--- book-store/src/components/books/BooksFilter.tsx
+++ book-store/src/components/books/BooksFilter.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useCategory } from '../../hooks/useCategory';
+import Button from '../common/Button';
+import { useSearchParams } from 'react-router-dom';
 
 function BooksFilter() {
+    const { category } = useCategory();
+
     return (
-        <div>
-            
-        </div>
+        <BooksFilterStyle>
+            <div className="category">
+                {
+                    category.map((item) => (
+                        <Button size="medium" scheme="normal" key={item.category_id}>{item.category_name}</Button>
+                    ))
+                }
+            </div>
+            <div className="new">
+                <Button size="medium" scheme='normal'>신간</Button>
+            </div>
+        </BooksFilterStyle>
     );
 }
 
-const BooksFilterStyle = styled.div``;
+const BooksFilterStyle = styled.div`
+    display: flex;
+    gap: 24px;
+
+    .category{
+        display: flex;
+        gap: 8px;
+    }
+`;
 
 export default BooksFilter;
