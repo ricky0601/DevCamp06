6bbda79bfda4d70872a36520dc09bb164cbd76c5
1dg <ricky2425@naver.com>
2025-04-29 23:31:55 +0900

    refactor: 리펙토링

--- book-store/src/components/books/BookItem.tsx
+++ book-store/src/components/books/BookItem.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import styled from 'styled-components';
-import { Book } from '../../models/book.model';
-import { getImgSrc } from '../../utils/image';
-import { formatNumber } from '../../utils/format';
+import { Book } from '@/models/book.model';
+import { getImgSrc } from '@/utils/image';
+import { formatNumber } from '@/utils/format';
 import { FaHeart } from 'react-icons/fa6';
-import { ViewMode } from './BooksViewSwitcher';
+import { ViewMode } from '@/components/books/BooksViewSwitcher';
 import { Link } from 'react-router-dom';
 
 interface BookItemProps {
